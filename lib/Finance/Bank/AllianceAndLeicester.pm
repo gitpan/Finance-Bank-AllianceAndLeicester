b07a431054669abba6be2e8fdf9057fdb44f617a
Simon Dawson <tehsi@cpan.org>
2009-06-11 07:50:05 +0000

    Import of TEHSI/Finance-Bank-AllianceAndLeicester-1.03 from CPAN.
    gitpan-cpan-distribution: Finance-Bank-AllianceAndLeicester
    gitpan-cpan-version:      1.03
    gitpan-cpan-path:         TEHSI/Finance-Bank-AllianceAndLeicester-1.03.tar.gz
    gitpan-cpan-author:       TEHSI
    gitpan-cpan-maturity:     released

--- lib/Finance/Bank/AllianceAndLeicester.pm
+++ lib/Finance/Bank/AllianceAndLeicester.pm
@@ -1,144 +1,160 @@
 package Finance::Bank::AllianceAndLeicester;
+
 use strict;
 use Carp;
-our $VERSION = '1.02';
-
-use WWW::Mechanize;
 use HTML::TokeParser;
+use WWW::Mechanize;
+
+our $VERSION = '1.03';
 
 sub check_balance {
-    my ($class, %opts) = @_;
-    my @accounts;
-    croak 'Must provide a Customer ID'	       unless exists $opts{customerid};
-    croak 'Must provide Memorable information' unless exists $opts{memorable};
-    croak 'Must provide a Unique Phrase'       unless exists $opts{phrase};
-    croak 'Must provide a PIN Code'            unless exists $opts{pin};
-    croak 'Customer ID should be 12 digits'    unless $opts{customerid} =~ /^\d{12}$/;
-    croak 'PIN Code should be 5 digits'        unless $opts{pin} =~ /^\d{5}$/;
 
-    my $self = bless { %opts }, $class;
-    
-    # Submit Customer ID
-    my $agent = WWW::Mechanize->new();
-    $agent->get("https://www.mybank.alliance-leicester.co.uk/login/checkLogin.asp?txtCustomerID=$opts{customerid}");
+  # Get the inputs, and perform sanity checking.
+  my ($class, %opts) = @_;
+  croak 'Must provide a customer ID' unless exists $opts{customerid};
+  croak 'Must provide memorable information' unless exists $opts{memorable};
+  croak 'Must provide a unique phrase' unless exists $opts{phrase};
+  croak 'Must provide a PIN code' unless exists $opts{pin};
+  croak 'Customer ID should be 8 digits' unless $opts{customerid} =~ /^\d{8}$/;
+  croak 'PIN code should be 5 digits' unless $opts{pin} =~ /^\d{5}$/;
 
-    # Submit the memorable information
-    $agent->get("https://www.mybank.alliance-leicester.co.uk/login/checkPM5.asp?txtMemDetail=$opts{memorable}");
+  # Initialise the output array.
+  my @accounts;
 
-    # Check we have got the correct Unique Phrase
-    my $content = $agent->content;
-    my $stream = HTML::TokeParser->new(\$content) or die "$!";
-    for (my $a=0; $a<5; $a++) {
-        $stream->get_tag("span");
+  # Submit the customer ID.
+  my $agent = WWW::Mechanize->new();
+  $agent->get('https://www.mybank.alliance-leicester.co.uk/index.asp');
+  $agent->submit_form(fields    => {txtCustomerID => $opts{customerid}},
+                      form_name => 'login');
+  croak 'Login failed' unless $agent->success();
+
+  # Submit the memorable information, if necessary.
+  if ($agent->form_name('frmChangePIN')) {
+    # We've got an extra step to perform.
+    $agent->submit_form(fields    => {txtMemDetail => $opts{memorable}},
+                        form_name => 'frmChangePIN');
+    croak 'Failed to submit memorable data' unless $agent->success();
+  }
+
+  # Check we have got the correct unique phrase.
+  my $content = $agent->content;
+  my $stream = HTML::TokeParser->new(\$content) or die $!;
+  for (my $a=0; $a < 5; $a++) {
+    $stream->get_tag('span');
+  }
+  my $phrase = $stream->get_trimmed_text('/span');
+
+  croak 'Unique phrase mismatch' unless (lc($phrase) eq lc($opts{phrase}));
+
+  # Submit the PIN number.
+  $agent->submit_form(fields    => {txtCustomerPIN => $opts{pin}},
+                      form_name => 'frmPM4point1');
+  croak 'Failed to submit PIN' unless $agent->success();
+
+  # Check for login errors.
+  $content = $agent->content;
+  $stream = HTML::TokeParser->new(\$content) or die $!;
+  while (my $token = $stream->get_tag('div')) {
+    if ($token->[1]{'id'} && $token->[1]{'id'} eq 'error') {
+      my $error = $stream->get_trimmed_text('/div');
+      croak "Error during login: $error\n";
+      last; # This is just silly.
     }
-    my $phrase = $stream->get_trimmed_text('/span');
-   
-    croak "Unique Phrase Mismatch." unless(lc($phrase) eq lc($opts{phrase}));
-    
-    # Submit the PIN Number
-    $agent->get("https://www.mybank.alliance-leicester.co.uk/login/checkPM4point1.asp?txtCustomerPIN=$opts{pin}");
+  }
 
-    # Check for login errors
-    $content = $agent->content;
-    $stream = HTML::TokeParser->new(\$content) or die "$!";
-    while(my $token = $stream->get_tag('div')) {
-        if($token->[1]{'id'} eq 'error') {
-	    my $error = $stream->get_trimmed_text('/div');
-            croak "Error During Login: $error\n";
-            last;
-        }
-    }
+  # Save this data to parse later.
+  $content = $agent->{content};
 
-    # save this data to parse later
-    $content = $agent->{content};
+  # We have the data we need, so let's log out.
+  $agent->get('https://www.mybank.alliance-leicester.co.uk/' .
+              'login/calls/logout.asp');
 
-    # We have the data we need, so lets logout
-    $agent->get("https://www.mybank.alliance-leicester.co.uk/login/calls/logout.asp");
+  # Begin parsing the HTML.
+  $stream = HTML::TokeParser->new(\$content) or die $!;
+  $stream->get_tag('tr');
 
-    # Begin parsing the HTML
-    $stream = HTML::TokeParser->new(\$content) or die "$!";
-    $stream->get_tag("tr");
+  while (my $token = $stream->get_tag('tr')) {
 
-    while (my $token = $stream->get_tag('tr')) {
+    my($balance, $name, $overdraft, $account, $available_balance);
 
-        my($accountbalance, $accountname, 
-            $accountoverdraft, $accountnumber, $accountavailable);
+    # Get the account name and number.
+    $token = $stream->get_tag('td');
+    $name = $stream->get_trimmed_text('/td');
+    $name =~ s/\s(\d+)$//;
+    $account = $1;
 
-	# Get the Account Name and number
-	$token = $stream->get_tag('td');
-	$accountname = $stream->get_trimmed_text('/td');
-	$accountname =~ s/\s(\d+)$//;
-	$accountnumber = $1;
+    # Get the balance.
+    $stream->get_tag('td');
+    $balance = $stream->get_trimmed_text('/td');
 
-	# Get the balance
-	$stream->get_tag('td');
-	$accountbalance = $stream->get_trimmed_text('/td');
+    # Get the overdraft limit.
+    $stream->get_tag('td');
+    $overdraft = $stream->get_trimmed_text('/td');
 
-        # Get overdraft
-	$stream->get_tag('td');
-	$accountoverdraft = $stream->get_trimmed_text('/td');
+    # Fix up the overdraft amount to zero, if there is no overdraft available.
+    $overdraft = 0 if ($overdraft eq 'n/a');
 
-	# Get Available Balance
-	$stream->get_tag('td');
-        $accountavailable = $stream->get_trimmed_text('/td');
+    # Get the available balance.
+    $stream->get_tag('td');
+    $available_balance = $stream->get_trimmed_text('/td');
 
-	# Strip pounds signs from balances
-	$accountbalance   =~ s/^\x{00A3}//;
-	$accountoverdraft =~ s/^\x{00A3}//;
-        $accountavailable =~ s/^\x{00A3}//;
+    # Strip pounds signs from balances.
+    $balance   =~ s/^\x{00A3}//;
+    $overdraft =~ s/^\x{00A3}//;
+    $available_balance =~ s/^\x{00A3}//;
 
-	# Strip Comma ',' from balances
-	$accountbalance   =~ s/\,//g;
-        $accountoverdraft =~ s/\,//g;
-        $accountavailable =~ s/\,//g;
-                   
-        # Add to list of accounts to return
-        push @accounts, {
-            balance           => $accountbalance,
-            name              => $accountname,
-            overdraft         => $accountoverdraft,
-	    account           => $accountnumber,
-            available_balance => $accountavailable
-        };
-    }
+    # Strip Comma ',' from balances.
+    $balance   =~ s/\,//g;
+    $overdraft =~ s/\,//g;
+    $available_balance =~ s/\,//g;
 
-    # Return the list of accounts
-    return @accounts;
+    # Add to list of accounts to return.
+    push(@accounts, {balance           => $balance,
+                     name              => $name,
+                     overdraft         => $overdraft,
+                     account           => $account,
+                     available_balance => $available_balance});
+  }
+
+  return @accounts;
 }
 
 1;
+
 __END__
-# Documentation is below here
 
 =head1 NAME
 
-Finance::Bank::AllianceAndLeicester - Check your Alliance & Leicester bank accounts from Perl
+Finance::Bank::AllianceAndLeicester - Check your Alliance & Leicester bank
+accounts from Perl
 
 =head1 SYNOPSIS
 
   use Finance::Bank::AllianceAndLeicester;
   my @accounts = Finance::Bank::AllianceAndLeicester->check_balance(
-      customerid  => '012345678912',
+      customerid  => '01234567',
       pin         => '12345',
       memorable   => 'mybirthplace',
       phrase      => 'my unique phrase'
   );
 
   foreach (@accounts) {
-      printf "%12s(%20s):  GBP %8.2f (Overdraft: GBP %8.2f Available: GBP %8.2f)\n",
-        $_->{account}, $_->{name}, $_->{balance}, $_->{overdraft}, $_->{available_balance} ;
+      printf("%8s(%20s):  GBP %8.2f " .
+             "(Overdraft: GBP %8.2f Available: GBP %8.2f)\n",
+             $_->{account}, $_->{name},
+             $_->{balance}, $_->{overdraft}, $_->{available_balance});
   }
 
 =head1 DESCRIPTION
 
 This module provides a rudimentary interface to the Alliance & Leicester
-online banking system at C<https://www.mybank.alliance-leicester.co.uk/>. 
+online banking system at L<https://www.mybank.alliance-leicester.co.uk/>.
 
 =head1 DEPENDENCIES
 
-You will need either C<Crypt::SSLeay> or C<IO::Socket::SSL> installed 
-for HTTPS support to work with LWP.  This module also depends on 
-C<WWW::Mechanize> and C<HTML::TokeParser> for screen-scraping.
+You will need either L<Crypt::SSLeay> or L<IO::Socket::SSL> installed
+for HTTPS support to work with LWP.  This module also depends on
+L<WWW::Mechanize> and L<HTML::TokeParser> for screen-scraping.
 
 =head1 CLASS METHODS
 
@@ -146,25 +162,24 @@ C<WWW::Mechanize> and C<HTML::TokeParser> for screen-scraping.
 
 =item B<check_balance>
 
-    check_balance ( customerid => $c, 
-                    pin        => $p, 
-                    memorable  => $m, 
-                    phrase     => $s 
-    )
+  check_balance ( customerid => $c,
+                  pin        => $p,
+                  memorable  => $m,
+                  phrase     => $s )
 
 Return an array of account hashes, one for each of your bank accounts.
 
 =item customerid
 
-The Customer ID is 12 Digit number supplied with your account
+The Customer ID is the 8-digit number supplied with your account.
 
 =item pin
 
-Your 5 Digit PIN Number
+Your 5-digit PIN number.
 
 =item memorable
 
-This is your memorable information. Such as your birth place. 
+This is your memorable information, such as your birth place.
 This is asked for when you first login to your bank account.
 
 =item phrase
@@ -172,25 +187,26 @@ This is asked for when you first login to your bank account.
 Your unique phrase. This is used to make sure we are connecting to
 the Alliance & Leicester website and that the connection has not been hijacked.
 
-This is created when you fisrt sign up your account, and can be changed on the 
-Alliance & Leicester internet banking website.
+This is created when you first sign up for online access to your account, and
+can be changed on the Alliance & Leicester internet banking website.
 
 =back
 
-=head1 ACCOUNT HASH KEYS 
+=head1 ACCOUNT HASH KEYS
 
-    $ac->account
-    $ac->name
-    $ac->balance
-    $ac->overdraft
-    $ac->available_balance
- 
-Return the account number, account name (eg. 'PlusSaver'), account
-balance, account overdraft limit and available balance as a signed floating point value.
+  $ac->account
+  $ac->name
+  $ac->balance
+  $ac->overdraft
+  $ac->available_balance
+
+Return the account number, account name (e.g. 'PlusSaver'), account
+balance, account overdraft limit and available balance as a signed floating
+point value.
 
 =head1 WARNING
 
-This warning is from Simon Cozens' C<Finance::Bank::LloydsTSB>, and seems
+This warning is from Simon Cozens' L<Finance::Bank::LloydsTSB>, and seems
 just as apt here.
 
 This is code for B<online banking>, and that means B<your money>, and
@@ -201,19 +217,25 @@ to me, but is provided under B<NO GUARANTEE>, explicit or implied.
 
 =head1 THANKS
 
-Simon Cozens for C<Finance::Bank::LloydsTSB> and Chris Ball for Finance::Bank::HSBC, 
-upon which most of this code is based. 
-Andy Lester (and Skud, by continuation) for WWW::Mechanize, Gisle Aas for HTML::TokeParser.
+Simon Cozens for L<Finance::Bank::LloydsTSB> and Chris Ball for
+Finance::Bank::HSBC, upon which most of this code is based.
+Andy Lester (and Skud, by continuation) for L<WWW::Mechanize>, Gisle Aas for
+L<HTML::TokeParser>.
 
 =head1 CHANGELOG
 
 =over
 
-=item Version 1.02 - 10/10/2006 - Ian Bissett <ian@tekuiti.co.uk>
+=item Version 1.03 - 08/06/2009 - Simon Dawson L<tehsi@cpan.org>
 
-* Reduced PERL version in Makefile.PL to 5.006001 which should solve some installation issues.
+* Rewrote to work with most-recent Alliance & Leicester website.
 
-=item Version 1.01 - 04/10/2006 - Ian Bissett <ian@tekuiti.co.uk>
+=item Version 1.02 - 10/10/2006 - Ian Bissett L<ian@tekuiti.co.uk>
+
+* Reduced PERL version in Makefile.PL to 5.006001 which should solve some
+installation issues.
+
+=item Version 1.01 - 04/10/2006 - Ian Bissett L<ian@tekuiti.co.uk>
 
 * Strip commas (',') from balances.
 
@@ -221,7 +243,7 @@ Andy Lester (and Skud, by continuation) for WWW::Mechanize, Gisle Aas for HTML::
 
 =head1 AUTHOR
 
-Ian Bissett C<ian.bissett@tekuiti.co.uk>
+Originally written by Ian Bissett L<ian.bissett@tekuiti.co.uk>;
+currently maintained by Simon Dawson L<tehsi@cpan.org>.
 
 =cut
-
